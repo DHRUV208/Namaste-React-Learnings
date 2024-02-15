5fdc91d1f7cdacc79804fccf70dc97d91a807d8c
dhruv.mehta <dhruv.mehta@sequelstring.com>
2024-02-15 15:07:57 +0530

    first commit

---
+++ Episode-1/App.js
@@ -0,0 +1,12 @@
+const heading3 = React.createElement(
+    "h1",
+    {id: "heading"},
+    "Namaste React"
+    );
+
+    console.log(heading3);
+const root3 = ReactDOM.createRoot(document.getElementById("root"));
+
+root3.render(heading3);
+
+const parent = React
---
+++ Episode-1/Index.js
@@ -0,0 +1,16 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Nested React"),
+    React.createElement("h1", {}, "siblings"),
+  ]),
+React.createElement("div", {id: "child2"}, [
+    React.createElement("h1", {}, "H1 child2"),
+    React.createElement("h1", {}, "h1 sibling child2")
+])]
+);
+
+const root1 = ReactDOM.createRoot(document.getElementById("root"));
+console.log(parent);
+root1.render(parent);
