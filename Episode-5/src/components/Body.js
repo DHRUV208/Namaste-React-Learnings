7afddb3f819cafc22c27b6d23e736992d7a114b5
dhruv.mehta <dhruv.mehta@sequelstring.com>
2024-02-26 21:30:37 +0530

    Hooks, React Reconciliation, React Fiber

--- Episode-5/src/components/Body.js
+++ Episode-5/src/components/Body.js
@@ -1,11 +1,22 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 
 const Body = () => {
+
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  //      variable            function               utility function by react
+  const arr = useState(resList);
+  const list = arr[0];
+  const setList = arr[1];
+
     return (
       <div className="body">
-        <div className="search">Search</div>
+        <div className="filter">
+        <button className="filter-btn" onClick={()=>{console.log("btn clicked")}}>Top rated restaurant</button>
+
+        </div>
         <div className="rest-container">
           {resList.map((restaurant) => (
             <RestaurantCard key={restaurant.data.id} resData={restaurant} />
